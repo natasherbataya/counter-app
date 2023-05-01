d6d7b61255ae5d24f14948af11b725994b3025f7
natasherbataya <125377783+natasherbataya@users.noreply.github.com>
2023-05-01 19:29:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let buttonAdd = document.querySelector('.btn-add');
+let buttonReset = document.querySelector('.btn-reset');
+let counterText = document.querySelector('.counter');
+
+let counter = 0;
+
+
+buttonAdd.addEventListener('click', function() {
+    counter = counter + 1;
+    
+    counterText.innerText = counter;
+});
+
+
+buttonReset.addEventListener('click', function() {
+    counter = 0;
+    
+    counterText.innerText = counter;
+});
+
